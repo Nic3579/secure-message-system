180ca75f347cd7d741958edf17b3474117a514b5
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-12-17 14:03:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,11 @@ input.onButtonPressed(Button.A, function () {
     radio.sendValue("A", number)
 })
 radio.onReceivedValue(function (name, value) {
+    // if this is the receiving micro:bit, it will send back the value multiplied by 696969
     if (name == "A") {
         radio.sendValue("B", value * 696969)
     } else if (name == "B") {
+        // if this is the sending micro:bit, check to see if good connection and show appropriate image
         if (value / 696969 == number) {
             basic.showIcon(IconNames.Yes)
         } else {
